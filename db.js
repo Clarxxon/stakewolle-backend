fdfd378fbc44b04c2044f203616d3b85e9efa9fc
Abdullah Shirinov <www.sar.ras@gmail.com>
2022-05-05 22:15:29 +0300

    deploy

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize(
-		process.env.DB_NAME,
-		process.env.DB_USER,
-		process.env.DB_PASSWORD,
+		process.env.DB_NAME || "stakewolle",
+		process.env.DB_USER || "postgres",
+		process.env.DB_PASSWORD || "123456789",
 		{
 				dialect: 'postgres',
-				host: process.env.DB_HOST,
+				host: process.env.DB_HOST || "localhost",
 				port: process.env.DB_PORT
 		}
 
