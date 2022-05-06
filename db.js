a082e3cb6d393ca4c5d8fdad69de6617097e6212
Abdullah Shirinov <www.sar.ras@gmail.com>
2022-05-06 11:15:33 +0300

    deploy

--- db.js
+++ db.js
@@ -1,13 +1,21 @@
 const { Sequelize } = require('sequelize');
+const env = process.env.NODE_ENV || "development"
+const config = require('./config/config.json')[env]
 
-module.exports = new Sequelize(
-		process.env.DB_NAME,
-		process.env.DB_USER,
-		process.env.DB_PASSWORD,
-		{
-				dialect: 'postgres',
-				host: process.env.DB_HOST,
-				port: process.env.DB_PORT
-		}
+if (config.use_env_variable) {
+		module.exports = new Sequelize(process.env[config.use_env_variable])
+} else {
+		module.exports = new Sequelize(config.database, config.username, config.password, config)
+}
 
-)
+// module.exports = new Sequelize(
+// 		process.env.DB_NAME,
+// 		process.env.DB_USER,
+// 		process.env.DB_PASSWORD,
+// 		{
+// 				dialect: 'postgres',
+// 				host: process.env.DB_HOST,
+// 				port: process.env.DB_PORT
+// 		}
+//
+// )
