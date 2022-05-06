2ee23fd4682c6a17fc4171f9e3cbdbd8f4d18ce1
Abdullah Shirinov <www.sar.ras@gmail.com>
2022-05-06 10:24:26 +0300

    deploy

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 const { Sequelize } = require('sequelize');
 
-const isProduction = process.env.NODE_ENV === 'production'
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-
 module.exports = new Sequelize(
-		isProduction
-				? process.env.DATABASE_URL
-				: connectionString,
-		ssl: isProduction
+		process.env.DB_NAME,
+		process.env.DB_USER,
+		process.env.DB_PASSWORD,
+		{
+				dialect: 'postgres',
+		}
 
 )
