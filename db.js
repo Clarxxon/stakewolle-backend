a143b09eb8feb80b2db4a229e5d07d8b5652d8b8
Abdullah Shirinov <www.sar.ras@gmail.com>
2022-05-06 10:06:27 +0300

    deploy

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require('sequelize');
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = new Sequelize(
+		isProduction
+				? process.env.DATABASE_URL
+				:
 		process.env.DB_NAME,
 		process.env.DB_USER,
 		process.env.DB_PASSWORD,
